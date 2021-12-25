df5d7b87e0b0fd84c211459ce2cd011404f071a9
Emmanuel Carreira Alves <emmanuel_carreira@hotmail.com>
2021-12-25 01:07:58 +0000

    refactor: changes search_by_username to return User instance

--- src/dao/user_dao.py
+++ src/dao/user_dao.py
@@ -6,10 +6,11 @@ class UserDAO:
     def __init__(self, manager: Postgres):
         self.manager = manager
 
-    def search_by_username(self, username: str):
-        query = "SELECT u FROM users u WHERE u.name = %s"
+    def search_by_username(self, username: str) -> User:
+        query = "SELECT * FROM users u WHERE u.name = %s"
         self.manager.execute_sql_command(query, (username,))
-        return self.manager.cursor.fetchone()
+        fetched_user = self.manager.cursor.fetchone()
+        return User(*fetched_user) if fetched_user else None
 
     def delete_user(self, user: User):
         pass
